c800e7cdfc8f471ef01235a1f55b0ec0c003b42d
Abdullah Alibaks <abdullah.alibaks@student.rocva.nl>
2023-02-20 23:48:07 +0100

    Foto toont maar allemaal dezelfde

--- App.js
+++ App.js
@@ -5,19 +5,20 @@ import { launchCamera } from 'react-native-image-picker';
 const App = () => {
   const [players, setPlayers] = useState([]);
   const [name, setName] = useState('');
+  const [foto, setFoto ] = useState(null);
   const [position, setPosition] = useState('');
-  const [rugnummer, setRugnummer] = useState('')
-  const [image, setImage ] = useState("null");
+  const [rugnummer, setRugnummer] = useState('');
+
   
   const addPlayer = () => {
-    setPlayers([...players, { name,position, rugnummer, image:null }]);
+    setPlayers([...players, { name, position, rugnummer, foto: null }]);
     setName('');
+    setFoto(null);
     setPosition('');
     setRugnummer('');
-    setImage(null)
   };
 
-  const openCamera = () => {
+   camlaunch = () => {
     let options = {
       storageOptions: {
         skipBackup: true,
@@ -25,12 +26,11 @@ const App = () => {
       },
     };
     launchCamera(options, (res) => {
-      setImage(res);
-      console.log('response = ', res.assets[0].uri);
-
+      console.log('response = ', res);
+      setFoto(res);
+      console.log('foto = ', foto);
     });
   }
-
   return (
     <View style={styles.container}>
       <TextInput
@@ -38,22 +38,22 @@ const App = () => {
         value={name}
         onChangeText={setName}
         placeholder="Naam"
-        keyboardType="default"
       />
       <TextInput
         style={styles.input}
         value={position}
         onChangeText={setPosition}
         placeholder="Positie"
-        keyboardType="default"
       />
       <TextInput
         keyboardType='numeric'
         style={styles.input}
         value={rugnummer}
         onChangeText={setRugnummer}
-        placeholder="Voer het Rugnummer in"
+        placeholder="rugnummer"
+        
       />
+      
 
       <TouchableOpacity onPress={addPlayer} style={styles.button}>
         <Text style={{ color: '#fff' }}>Add Player</Text>
@@ -63,11 +63,11 @@ const App = () => {
       data={players}
       renderItem={({item}) => (
         <View style={styles.playerContainer}>
-          <View style={{height:60, width:60,backgroundColor:'black' }}>
-          {item.image && item.image.uri && (
+          <View>
+          {foto && (
           <Image
-              source={{  uri:item.image }}
-              style={{width:60, height:60,borderRadius:30 }}
+              source={{  uri:foto.assets[0].uri }}
+              style={{width:70, height:80,borderRadius:30, borderColor: "black", borderWidth:2 }}
             />
           )}
           </View>
@@ -76,13 +76,14 @@ const App = () => {
             <Text>{item.position}</Text>
             <Text>{item.rugnummer}</Text>
           </View>
+          
           <Button
+            style={{borderRadius:20}}
             title="Take Picture"
-            onPress={(openCamera)}
+            onPress={camlaunch}
           />
         </View>
       )}
-      
           />
 </View>
   );
@@ -95,15 +96,19 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
+    backgroundColor: 'darkgrey',
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 16,
   },
   button: {
-    backgroundColor: 'blue',
+    marginTop:15,
+    backgroundColor: 'green',
     padding: 12,
+    borderRadius:10,
   },
   playerContainer: {
+    marginTop: 60,
     height: 70,
     flexDirection: 'row',
     marginBottom: 16,
@@ -111,7 +116,6 @@ const styles = StyleSheet.create({
   },
   playerName: {
     fontSize: 18,
-    color: "white",
   },
 });
 
